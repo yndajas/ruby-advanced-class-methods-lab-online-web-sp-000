040d60e2e09ece950415894c86e7d2b0a3da315e
Ynda Jas <yndajas@gmail.com>
2020-07-17 15:41:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,8 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    return self.find_by_name(name) if true
+    if self.find_by_name(name)
+      self.find_by_name(name)
     else
       self.create_by_name(name)
     end
